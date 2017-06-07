5b9b9a3daf8cb32e0bd7ab0408a4df0f58cb815a
Jaakko Palomäki <jaakko.palomaki@gmail.com>
2017-06-07 15:25:56 +0300

    7.6.2017 - 15:25
    Placeholder for obstacles, control keybinds.

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ var canvas = document.getElementById("canvas"),
        
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+/*function Obstacle(I) {					PLACEHOLDER
+	I.active = true;
+	I.color = "white";
+	I.y = 0;
+	I.width = 30;
+	I.height = Math.random() * 1000 + 1;
+	
+	I.draw = function() {
+		this.draw()
+	}
+}*/
+
+
+function controls() {
+	if(event.keyCode == 39) // Right arrow
+		this.player.x += 1;
+	if(event.keyCode == 37) // Left Arrow
+		this.player.x -= 1;
+	if(event.keyCode == 38) // Up Arrow
+		this.player.y -= 4;
+	if(event.keyCode == 40) // Down arrow
+		this.player.y += 4;
+	if(event.keyCode == 32) //Spacebar
+		this.player.y += 40; 
+}
