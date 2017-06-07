5d5ec532f04e01709f991012a2d6d5c706295d92
Marjanaama <santeri.joki@gmail.com>
2017-06-07 13:33:29 +0300

    7.6.2017 - 13.33
    Fixed another typo

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ var canvas = document.getElementById("canvas"),
        ctx = canvas.getContext("2d");
        
 canvas.width = window.innerWidth;
-canvas.heigth = window.innerHeight;
+canvas.height = window.innerHeight;
