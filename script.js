301af9d162978799099d7a46e0232225a0672073
Marjanaama <santeri.joki@gmail.com>
2017-06-07 15:40:33 +0300

    7.6.2017 - 15.39
    -Created game function to serve as a base
    -Created couple of animations for the player
    -Created basic animation loop

--- script.js
+++ script.js
@@ -29,3 +29,73 @@ function controls() {
 	if(event.keyCode == 32) //Spacebar
 		this.player.y += 40; 
 }
+
+Game = function(){
+	this.player = {
+       x: 100,
+       y: 100,
+       w: 52,
+	   h: 60,
+	   runw: 56,
+	   runh: 72,
+	   diry: 1,
+	   dirx: 1,
+	   movestat: 1,
+	   idleframe: 0,
+	   idleframemax: 3,
+	   idletick: 0,
+	   idletickmax: 11,
+	   runframe: 0,
+	   runframemax: 5,
+	   runtick: 0,
+	   runtickmax: 6,
+	   
+       img: null
+	};
+	this.img = new Image();
+	this.img.onload = function() {
+		game.player.img = game.img;
+	}
+	this.img.src = "sprites/player/player_spritesheet.png";
+	this.player.playeranimate = function(){
+		if (this.img === null)
+			return;
+		switch(this.movestat){
+			case 0:	
+					ctx.drawImage(this.img,18+(this.idleframe*64),24,26,30,this.x,this.y,this.w,this.h);
+					if (this.idletick <= this.idletickmax){
+						this.idletick++;
+					}
+					else{
+						this.idletick = 0;
+						this.idleframe++;
+					}
+					if (this.idleframe >= this.idleframemax){
+						this.idleframe = 0;
+					}
+					break;
+			case 1:	
+					ctx.drawImage(this.img,18+(this.runframe*64),82,30,36,this.x,this.y-12,this.runw,this.runh);
+					if (this.runtick <= this.runtickmax){
+						this.runtick++;
+					}
+					else{
+						this.runtick = 0;
+						this.runframe++;
+					}
+					if (this.runframe >= this.runframemax){
+						this.runframe = 0;
+					}
+					break;
+			default:
+		}
+	}
+
+	this.animate = function(){
+		requestAnimationFrame(game.animate);
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		game.player.playeranimate();
+	}
+}
+var game = new Game();
+window.onload = game.animate();
